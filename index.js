52b91c243edf9a1980c28eeb837a112fbfa8a5cc
Anthony Kumasaka <anthonykumasaka@gmail.com>
2018-07-29 23:05:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  var id = document.getElementById("nested")
+  var nestedElement = id.getElementsByClassName("target")
+  return nestedElement; 
+}
+
+function increaseRankBy(n) {
+  const rank = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt((i + n).toString()); 
+  }
+}
+
+function deepestChild() {
+  
+}
